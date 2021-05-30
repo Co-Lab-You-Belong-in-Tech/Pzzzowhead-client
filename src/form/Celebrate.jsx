056a015bafd3be18b98a09cf48300a5e00c42a18
Andrew Bray <55928908+Andrew-Bray@users.noreply.github.com>
2021-05-30 16:02:26 -0700

    Update Celebrate.jsx

--- src/form/Celebrate.jsx
+++ src/form/Celebrate.jsx
@@ -15,7 +15,7 @@ const Celebrate = () => {
       <div className="celebrate-card form">
         <h2>Woohoo!</h2>
         <p>You're all set up and I just sent you a text</p>
-        <img src="http://www.placekitten.com/310/200" alt="celebrate-pic" />
+        <img src="https://www.placekitten.com/310/200" alt="celebrate-pic" />
         <p>I'm SO excited you're joining  me on this 3 week journey to sleep on time and wake up with energy!</p>
         <button onClick={navigateHome}>Great!</button>
       </div>
