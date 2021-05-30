8810054b56ce93023771db386cb2fdaead6ff7a7
Andrew Bray <55928908+Andrew-Bray@users.noreply.github.com>
2021-05-30 16:03:01 -0700

    Update Ready.jsx

--- src/form/Ready.jsx
+++ src/form/Ready.jsx
@@ -14,7 +14,7 @@ const Ready = () => {
     <>
       <div className="ready-card form">
         <h2>Ready to sleep Earlier?</h2>
-        <img src="http://www.placekitten.com/300/200" alt="ready-pic" />
+        <img src="https://www.placekitten.com/300/200" alt="ready-pic" />
         <p>I'm your personal textbot, <em>Pzzzowhead</em> and will be texting you wind-down and bedtime reminders!</p>
         <p>Let's create your custom sleep schedule.</p>
 
