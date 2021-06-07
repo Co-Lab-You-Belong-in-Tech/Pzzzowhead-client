62498a50fe8554d0c4b236a4af18f835c2b0cb72
Andrew-Bray <amichaelbray@gmail.com>
2021-06-07 15:58:51 -0700

    Aos scroll animation

--- src/components/SmsImage.jsx
+++ src/components/SmsImage.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import styles from '../styles/SmsImage.css'
+import React, { useEffect } from 'react';
+import styles from '../styles/SmsImage.module.css'
+import Aos from 'aos';
+require('aos/dist/aos.css');
 
 // IMAGES 
 import phoneScreen from '../assets/iPhone_Mockup/Group\ 77.png';
@@ -14,22 +16,37 @@ import faceTwo from '../assets/iPhone_Mockup/face_2.png';
 
 
 const SmsImage = () => {
+  // Animation prep
+  useEffect(() => {
+    Aos.init({duration: 1000, 
+      delay: 800 
+    });
+  }, [])
   return (
-    <div className={styles.smsImageContainer}>
-      <div className={`${styles.smsContainer} ${styles.textOne}`}>
+    <div data-aos="fade-up" className={styles.smsImageContainer}>
+      <div data-aos="fade-left"
+        data-aos-delay="1000" className={`${styles.smsContainer} ${styles.textOne}`}>
         <img src={RectangleTwo} alt="text bubble One" className={styles.textBubble} />
         <img src={textTwo} alt="text content One" className={styles.textContent} />
         <img src={clockEmoji} alt="clock-emoji" className={styles.clockEmoji} />
         <div className={styles.faceOne}>
-          <img src={faceOne} alt="smiley face" />
+          <img data-aos="zoom-in" 
+           data-aos-easing="ease-out-back"
+           data-aos-delay="3000"
+            src={faceOne} alt="smiley face" />
         </div>
       </div>
-      <div className={`${styles.smsContainer} ${styles.textTwo}`}>
+      <div data-aos="fade-right" 
+        data-aos-delay="1000"className={`${styles.smsContainer} ${styles.textTwo}`}>
         <img src={RectangleOne} alt="text bubble Two" className={styles.textBubble} />
         <img src={textOne} alt="text content Two" className={styles.textContent} />
         <img src={strongEmoji} alt="clock-emoji" className={styles.strongEmoji} />
         <div className={styles.faceTwo}>
-          <img src={faceTwo} alt="smiley face" />
+          <img 
+            data-aos="zoom-in" 
+            data-aos-easing="ease-out-back"
+            data-aos-delay="3000"
+            src={faceTwo} alt="smiley face" />
         </div>
       </div>
       <div className={`${styles.faceContainer} ${styles.faceOne}`}>
