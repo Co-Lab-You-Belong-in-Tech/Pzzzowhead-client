0da2b58526362b6ae5dd79f9534aeb138f729359
Andrew-Bray <amichaelbray@gmail.com>
2021-06-10 17:06:00 -0700

    sooo close

--- src/form/Ready.jsx
+++ src/form/Ready.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import Aos from 'aos';
 import styles from '../styles/Ready.module.css';
 
 
 import readyGif from '../assets/pzzzow-animation-1.gif';
 
+
 const Ready = () => {
+  useEffect(() => {
+    Aos.init();
+  }, []);
 
   const history = useHistory();
 
@@ -19,12 +24,12 @@ const Ready = () => {
   return (
     <>
       <div className={styles.readyContainer}>
-        <h2>Ready to sleep Earlier?</h2>
-        <img src={readyGif} alt="ready-pic" />
-        <p>I'm your personal textbot, <em >pzzzow</em> and will be texting you wind-down and bedtime reminders!</p>
+        <h2 data-aos="fade-in"> Ready to sleep Earlier?</h2>
+        <img data-aos="fade-in" data-aos-duration="1000" src={readyGif} alt="ready-pic" />
+        <p data-aos="fade-in" >I'm your personal textbot, <em >pzzzow</em> and will be texting you wind-down and bedtime reminders!</p>
         <p>Let's create your custom sleep schedule.</p>
 
-        <button onClick={navigateToNameNumber}>I'm Ready</button>
+        <button className="bouncy" onClick={navigateToNameNumber}>I'm Ready</button>
 
       </div>
     </>
